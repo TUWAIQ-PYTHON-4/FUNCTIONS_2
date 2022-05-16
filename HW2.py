1c29f1af96ed4b2288715c22435852d583165256
Abdulaziz Altariqi <AbdulazizAlturaiqi@gmail.com>
2022-05-16 21:08:50 +0300

    Added output values

--- HW2.py
+++ HW2.py
@@ -4,10 +4,12 @@ def prime_num(first_num:int, second_num:int):
     This function used to printing prime numbers between two parameter
     '''
     for n in range(first_num, second_num + 1, 1):
-        for i in range(2,n):
+        for i in range(2, n, 1):
             if (n % i) == 0:
                 break
         else:
             print(n, end=" ")
 
 prime_num(25,50)            
+
+# OUTPUT: 29 31 37 41 43 47
